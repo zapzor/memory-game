8d3b89af9b44c3813374309b543a54ccfe615994
Zapzor <imp.4xlbm@aleeas.com>
2021-10-12 11:42:15 +0100

    oop

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,49 +5,24 @@ import Score from './Score';
 export default function Main() {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
-  const [arr, setArr] = useState([]);
-  // const [wasClicked, setWasClicked] = useState({
-  //   one: true,
-  //   two: true,
-  //   three: true,
-  //   four: true,
-  //   five: true,
-  //   six: true,
-  //   seven: true,
-  //   eight: true,
-  //   nine: true,
-  //   ten: true,
-  //   eleven: true,
-  //   twelve: true,
-  // });
-
-  // const [arr, setArr] = useState([]);
+  const [gameState, setGameState] = useState([]);
 
   useEffect(() => {
     setBestScore(score > bestScore ? score : bestScore);
   }, [score, bestScore]);
 
-  // function checkNumber(event) {
-  //   return wasClicked ? setScore(0) : score;
-  // }
-
   function checkNumber(props) {
-    console.log(arr.includes(props));
-    if (arr.includes(props)) {
+    if (gameState.includes(props)) {
       setScore(0);
-      setArr([]);
+      setGameState([]);
     } else {
       setScore(score + 1);
     }
   }
 
-  // function checkArr(props) {
-  //   arr.includes(props.target.value ? setScore(0) : score);
-  // }
-
   function handleClick(event) {
     const name = event.target.getAttribute('name');
-    setArr((prevArr) => [...prevArr, name]);
+    setGameState((prevArr) => [...prevArr, name]);
 
     checkNumber(name);
   }
