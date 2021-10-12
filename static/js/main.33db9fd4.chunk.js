ea7cdd3fc858314fda15ee36914ea3bb5e79e5c0
Zapzor <imp.4xlbm@aleeas.com>
2021-10-12 04:06:57 +0100

    Updates

---
+++ static/js/main.33db9fd4.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmemory-game"]=this["webpackJsonpmemory-game"]||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var r=n(1),c=n.n(r),o=n(3),i=n.n(o),s=(n(8),n(0));var d=function(){return Object(s.jsx)("div",{children:Object(s.jsx)("h1",{children:"Hello world!"})})};i.a.render(Object(s.jsx)(c.a.StrictMode,{children:Object(s.jsx)(d,{})}),document.getElementById("root"))},8:function(e,t,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.33db9fd4.chunk.js.map
