57f0bf9e27095aeca3fd534c1f85bfa8452c592a
Zapzor <imp.4xlbm@aleeas.com>
2021-10-13 04:08:35 +0100

    Board now randomzies on click

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,51 +1,18 @@
 import React from 'react';
 
 export default function Card(props) {
-  const { handleClick } = props;
+  const { handleClick, gameBoard } = props;
+  const bar = gameBoard.map((number) => {
+    return (
+      <li id={number} key={number} className='card' onClick={handleClick}>
+        {number}
+      </li>
+    );
+  });
 
   return (
     <div>
-      <ul className='cards'>
-        <div className='card' onClick={handleClick} name='one'>
-          1
-        </div>
-        <li className='card' onClick={handleClick} name='two'>
-          2
-        </li>
-        <li className='card' onClick={handleClick} name='three'>
-          3
-        </li>
-        <li className='card' onClick={handleClick} name='four'>
-          4
-        </li>
-        <li className='card' onClick={handleClick} name='five'>
-          5
-        </li>
-        <li className='card' onClick={handleClick} name='six'>
-          6
-        </li>
-        <li className='card' onClick={handleClick} name='seven'>
-          7
-        </li>
-        <li className='card' onClick={handleClick} name='eight'>
-          8
-        </li>
-        <li className='card' onClick={handleClick} name='nine'>
-          9
-        </li>
-        <li className='card' onClick={handleClick} name='ten'>
-          10
-        </li>
-        <li className='card' onClick={handleClick} name='eleven'>
-          11
-        </li>
-        <li className='card' onClick={handleClick} name='twelve'>
-          12
-        </li>
-      </ul>
+      <ul className='cards'>{bar}</ul>
     </div>
   );
 }
-
-// Increment score on clicking image that hasn't already been clicked
-// Reset score on clicking image that has already been clicked
--- src/components/Main.js
+++ src/components/Main.js
@@ -6,35 +6,38 @@ export default function Main() {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
   const [gameState, setGameState] = useState([]);
+  const [gameBoard, setGameBoard] = useState([
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+  ]);
 
   useEffect(() => {
     setBestScore(score > bestScore ? score : bestScore);
   }, [score, bestScore]);
 
-  function checkNumber(props) {
+  function shuffleArray(array) {
+    const shuffledArray = array.sort((a, b) => 0.5 - Math.random());
+    setGameBoard(shuffledArray);
+  }
+
+  function checkGameState(props) {
     if (gameState.includes(props)) {
       setScore(0);
       setGameState([]);
     } else {
       setScore(score + 1);
+      setGameState((prevArr) => [...prevArr, props]);
     }
   }
 
   function handleClick(event) {
-    const name = event.target.getAttribute('name');
-    setGameState((prevArr) => [...prevArr, name]);
-
-    checkNumber(name);
+    const number = event.target.id;
+    checkGameState(number);
   }
 
   return (
     <div>
       <Score score={score} bestScore={bestScore} />
-      <Card handleClick={handleClick} />
+      <Card handleClick={handleClick} gameBoard={gameBoard} />
     </div>
   );
 }
-
-// Jumble grid randomly on clicking image
-// Increment score on clicking image that hasn't already been clicked
-// Reset score on clicking image that has already been clicked
